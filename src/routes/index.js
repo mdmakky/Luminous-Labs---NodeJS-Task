536487d533e9e8f931e863b13cd36813442d71b4
makky <makky.cse@gmail.com>
2026-06-19 01:15:58 +0600

    feat: mount project and task routes in route index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import authRoutes from './auth.routes.js';
+import projectRoutes from './project.routes.js';
+import taskRoutes from './task.routes.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
+router.use('/projects', projectRoutes);
+router.use('/tasks', taskRoutes);
 
 export default router;
