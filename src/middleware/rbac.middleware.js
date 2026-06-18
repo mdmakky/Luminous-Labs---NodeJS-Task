3de08c68e4053bdc92f212a4eeadf9075997b5c1
makky <makky.cse@gmail.com>
2026-06-19 01:15:35 +0600

    feat: add RBAC middleware

---
+++ src/middleware/rbac.middleware.js
@@ -0,0 +1,39 @@
+import { ForbiddenError, UnauthorizedError } from '../utils/errors.js';
+
+// Check that the logged-in user has one of the allowed roles
+// Usage: requireRole('ADMIN', 'MANAGER')
+export const requireRole = (...roles) => {
+  return (req, _res, next) => {
+    if (!req.user) {
+      return next(new UnauthorizedError('Not authenticated'));
+    }
+    if (!roles.includes(req.user.role)) {
+      return next(new ForbiddenError('You do not have permission to perform this action'));
+    }
+    next();
+  };
+};
+
+// Block members from creating tasks (only ADMIN and MANAGER can create)
+export const requireCanCreateTask = (req, _res, next) => {
+  if (req.user?.role === 'MEMBER') {
+    return next(new ForbiddenError('Members are not allowed to create tasks'));
+  }
+  next();
+};
+
+// Block members from deleting tasks
+export const requireCanDeleteTask = (req, _res, next) => {
+  if (req.user?.role === 'MEMBER') {
+    return next(new ForbiddenError('Members are not allowed to delete tasks'));
+  }
+  next();
+};
+
+// Block members from creating projects
+export const requireCanManageProject = (req, _res, next) => {
+  if (req.user?.role === 'MEMBER') {
+    return next(new ForbiddenError('Members are not allowed to manage projects'));
+  }
+  next();
+};
